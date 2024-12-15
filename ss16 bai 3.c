d7acb32b261e7220fd27b029ddf7890995746656
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-12-15 22:07:22 +0700

    ss16 bai 3

---
+++ ss16 bai 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int sumnumber();
+	int arr[100]={1,2};
+	int *ptr = arr;
+	int x = *ptr;
+	printf("%d\n", x);
+	int y = *(ptr +1);
+	printf("%d\n", y);
+
+	printf("Ket qua : %d.", tong(&x, &y));
+	return 0;
+}
+
+int tong(int *a, int *b){
+	int sum;
+	sum = *a + *b;
+	return sum;
+}
