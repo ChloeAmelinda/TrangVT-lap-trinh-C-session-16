74aefc695d637320da675b5d46df4e24a12374d2
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-12-15 21:10:35 +0700

    ss16 bai 2

--- ss16 bai 2.cpp
+++ ss16 bai 2.cpp
@@ -1,9 +1,19 @@
 #include<stdio.h>
+void swap(int *input1, int *input2){
+	int temp =*input1;
+	*input1=*input2;
+	*input2=temp;
+} 
 int main(){
-	int n=6;
-	int *ptr=&n;
-	printf("gia tri ban dau la: %d\n",n);
-	*ptr= *ptr+10;
-	printf("Gia tri nam trong bien y la :%d\n",n);
+	int a,b;
+	printf("Nhap so a:");
+	scanf("%d",&a);
+	printf("Nhap so b:");
+	scanf("%d",&b);
+	printf("so a truoc khi doi cho la:%d\n",a);
+	printf("so b truoc khi doi cho la:%d\n",b);
+	swap(&a,&b);
+	printf("so a sau khi doi cho la:%d\n",a);
+	printf("so b sau khi doi cho la:%d\n",b);
 	return 0;
 }
