4ce7b0b26cb99262372ef92930d19eeb8d22e09a
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-12-15 20:28:30 +0700

    ss16 bai 1

--- ss16 bai 1.cpp
+++ ss16 bai 1.cpp
@@ -3,8 +3,8 @@ int main(){
 	int n; 
 	printf("Nhap gia tri bat ky:");
 	scanf("%d",&n);
-	printf("Gia tri phan tu : %d\n",&n);
+	printf("Gia tri phan tu : %d\n",n);
 	int *ptr=&n;
-	printf("Gia tri cua ptr: %d",ptr);
+	printf("Gia tri cua ptr: %d",*ptr);
 	return 0;
 }
